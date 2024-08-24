bf40f92572ca68531ce69893205c8ca8ce115db4
Ashish Gaikwad <work.ashish.gaikwad@gmail.com>
2024-08-24 22:19:06 +0530

    fix minor issue

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -19,7 +19,7 @@ const createUser = async (req, res) => {
 
     return res.status(201).json({ token });
   } catch (error) {
-    console.error("Error creating user:", error); // Add this line for debugging
+    console.error("Error creating user:", error);
     if (error instanceof z.ZodError) {
       return res.status(400).json({ errors: error.errors });
     }
